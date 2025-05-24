ddb2598cb6385ec5990d6a29624333573ee1fd05
Brunov Aleksey <lexa5978@yandex.ru>
2025-05-24 18:46:10 +0300

    v.2

--- actions/weather_actions.py
+++ actions/weather_actions.py
@@ -17,7 +17,7 @@ class ActionGetWeather(Action):
             dispatcher.utter_message(text="Не могу определить город для проверки погоды.")
             return []
 
-        api_key = "64240a2d35ef1acd0374d6f6f530739d"
+        api_key = "xx"
 
         try:
             base_url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric&lang=ru"
